09b6f95b4440ab7e495157826fbcf5928de25ff0
ChaitanyaValluru <Valluru.Chaitanya@ad.infosys.com>
2018-04-23 22:09:10 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,6 +27,23 @@ app.get('/todos', (req, res) => {
     }, (error) => {
         console.log(error);
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send('Invalid ID');
+    }
+    Todo.findById(id).then((todoById) => {
+        if(!todoById){
+            return res.status(404).send('No Id Found');
+        }
+        res.send({
+            todoById
+        });
+    }).catch((e) => {
+        res.status(400).send('Error in ID');
     });
 });
 
