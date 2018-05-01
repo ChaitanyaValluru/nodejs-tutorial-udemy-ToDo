bde14f6e810bfbd6f65b379b10c5fc4433809cb8
ChaitanyaValluru <Valluru.Chaitanya@ad.infosys.com>
2018-05-01 12:46:51 +0530

    Hash user password b4 saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,31 +1,39 @@
 const {SHA256} = require('crypto-js');
+var bcrypt = require('bcryptjs');
 
-var message = 'I am user Number 3';
+// var message = 'I am user Number 3';
+// var hash = SHA256(message).toString();
+// console.log(`Message: ${message}`);
+// console.log(`Hashed Value: ${hash}`);
 
-var hash = SHA256(message).toString();
+// var data = {
+//     id: 4
+// };
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'SomeSecret').toString()
+// };
 
-console.log(`Message: ${message}`);
-console.log(`Hashed Value: ${hash}`);
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
 
-
-var data = {
-    id: 4
-};
-var token = {
-    data,
-    hash: SHA256(JSON.stringify(data) + 'SomeSecret').toString()
-};
-
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(token.data)).toString();
-
-var resultHash = SHA256(JSON.stringify(token.data) + 'SomeSecret').toString();
-if(resultHash === token.hash){
-    console.log("Data is not changed");
-} else{
-    console.log("Data is changed");
-}
+// var resultHash = SHA256(JSON.stringify(token.data) + 'SomeSecret').toString();
+// if(resultHash === token.hash){
+//     console.log("Data is not changed");
+// } else{
+//     console.log("Data is changed");
+// }
 
 
+var password = 'abc123';
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// });
 
+var hashedPassword = '$2a$10$GaJuq6NMf5gRLCzN0jA./udB61poIgp9gAyAo80jP5KIoJ0LPE8uK';
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+});
 
